1e9890088e0c7fffe9b83b28b85699b07bd6f4ad
MasakazuKimpara <ebipilaf000@gmail.com>
2017-08-19 16:36:35 +0900

    新規プロジェクト作成 GemGenerator作成

---
+++ Assets/Component/Generator/Script/GemGanerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemGanerator : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_Gem;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
